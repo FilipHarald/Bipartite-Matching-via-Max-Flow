1667422f2bad23351c696b22b35cabb2d209038b
FilipHarald <filip.harald@gmail.com>
2015-10-29 16:40:43 +0100

    Added class Graph and some variables to Vertex and Edge

--- src/Edge.java
+++ src/Edge.java
@@ -1,4 +1,36 @@
 
 public class Edge {
+	Vertex start;
+	Vertex end;
+	int capacity;
+	int flow;
+	
+
+	public Edge(Vertex start, Vertex end, int capacity) {
+		this.start = start;
+		this.end = end;
+		this.capacity = capacity;
+		flow = 0;
+	}
+	
+	public Vertex getStart() {
+		return start;
+	}
+	
+	public Vertex getEnd() {
+		return end;
+	}
+	
+	public int getCapacity() {
+		return capacity;
+	}
+	
+	public int getFlow() {
+		return flow;
+	}
+	
+	public void setFlow(int flow) {
+		this.flow = flow;
+	}
 
 }
--- src/Vertex.java
+++ src/Vertex.java
@@ -1,4 +1,25 @@
-
 public class Vertex {
+	String name;
 
+	public Vertex(String name){
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+//	vet inte om denna kommer behövas
+//	@Override
+//	public boolean equals(Object obj) {
+//		if(obj instanceof Vertex){
+//			return name.equals(((Vertex)obj).name);
+//		}else{			
+//			return super.equals(obj);
+//		}
+//	}
 }
